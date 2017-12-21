d90ab06be64c0dffd814ed03e6db8f521fb244c7
Daniel Pereira <danielpp95@gmail.com>
2017-12-21 01:56:23 -0400

    clase 11

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -7,9 +7,15 @@ class Media extends Component {
     return (
       <div className='Media' >
         <div className="Media-cover " >
-          <img className="Media-image" src="./images/covers/bitcoin.jpg" alt="" width={260} height={160} />
-          <h3 className="Media-title" >¿por que aprender reacr?</h3>
-          <p className="Media-author" >daniel pereira</p>
+          <img 
+            className="Media-image" 
+            src={this.props.image} 
+            alt="" 
+            width={260} 
+            height={160} 
+          />
+          <h3 className="Media-title" > {this.props.title} </h3>
+          <p className="Media-author" > { this.props.author } </p>
         </div>  
       </div>
     )
