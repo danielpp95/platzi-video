e4b6fe84554c245771cfe59cc8560a7426cf1e79
Daniel Pereira <danielpp95@gmail.com>
2017-12-21 02:15:35 -0400

    clase 13

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import Media from '@/playlist/components/media'
 
 const app = document.getElementById('app')
 
-render(<Media title="marico el que lo lea" author="la puta esa" image="./images/covers/responsive.jpg" />, app)
+render(<Media type="video" title="marico el que lo lea" author="la puta esa" image="./images/covers/responsive.jpg" />, app)
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import './media.css'
 
 class Media extends Component {
+  // constructor(props) {
+  //   super(props)
+  //   this.handleClick = this.handleClick.bind(this)
+  // }
+  
+  handleClick = () => {
+    console.log(this.props.image)
+  }
   render() {
-
     return (
-      <div className='Media' >
+      <div className='Media' onClick={this.handleClick} >
         <div className="Media-cover " >
           <img 
             className="Media-image" 
@@ -20,6 +28,13 @@ class Media extends Component {
       </div>
     )
   }
+}
+
+Media.propTypes = {
+  image: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string,
+  type: PropTypes.oneOf(['video', 'audio'])
 }
 
 export default Media
