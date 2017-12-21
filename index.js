d801d0cccad763ee2bd3dcb7ff06fed7331ceb02
Daniel Pereira <danielpp95@gmail.com>
2017-12-21 01:15:24 -0400

    instancia basica del proyecto

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import {render} from 'react-dom'
+import Media from '@/playlist/components/media'
+
+const app = document.getElementById('app')
+
+render(<Media/>, app)
---
+++ src/playlist/components/media.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class Media extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <img 
+            src="" 
+            alt=""
+            width={260}
+            height={160}
+          />
+          <h3>¿por que aprender reacr?</h3>
+          <p>daniel pereira</p>
+        </div>  
+      </div>
+    )
+  }
+}
+
+export default Media
