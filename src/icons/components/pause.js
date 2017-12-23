6b7c6b4a9ccbce2ac562a3ad310a9f3eb56a7879
Daniel Pereira <danielpp95@gmail.com>
2017-12-23 00:11:37 -0400

    clase 19

---
+++ src/icons/components/pause.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Icon from './icon'
+
+function Play (props) {
+  return (
+    <Icon {...props}>
+      <path d="M4 4h10v24h-10zM18 4h10v24h-10z"></path>
+    </Icon>
+
+  )
+}
+
+export default Play
