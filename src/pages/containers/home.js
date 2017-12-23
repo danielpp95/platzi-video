8daa2863f89451b3bf196bb3e6adbed7e6e16cf2
Daniel Pereira <danielpp95@gmail.com>
2017-12-23 00:26:54 -0400

    clase 20

---
+++ src/pages/containers/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import HomeLayout from '@/pages/components/home-layout';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <HomeLayout />
+      </div>
+    );
+  }
+}
+
+export default Home;
